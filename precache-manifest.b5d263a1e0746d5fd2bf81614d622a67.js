9f6ff449ad57bffb3ba4062954116d728406dda1
suvrajit1998 <suvrajitnaskar1998@gmail.com>
2020-03-05 00:53:40 +1400

    Updates

---
+++ precache-manifest.b5d263a1e0746d5fd2bf81614d622a67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701a036b08c2e6a51b6a3ca4ea39ffaa",
+    "url": "/bakery/index.html"
+  },
+  {
+    "revision": "1571e65dd18d8d380ece",
+    "url": "/bakery/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "66b4856303c61143f21e",
+    "url": "/bakery/static/css/main.ae56ed69.chunk.css"
+  },
+  {
+    "revision": "1571e65dd18d8d380ece",
+    "url": "/bakery/static/js/2.1a44c936.chunk.js"
+  },
+  {
+    "revision": "5c984b27918dbad09d6fd060cae643c7",
+    "url": "/bakery/static/js/2.1a44c936.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66b4856303c61143f21e",
+    "url": "/bakery/static/js/main.49cedf11.chunk.js"
+  },
+  {
+    "revision": "9fc1fdc370daddfc289d",
+    "url": "/bakery/static/js/runtime-main.7590b9c1.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/bakery/static/media/11.2 shopping-bag.svg.30f07071.svg"
+  },
+  {
+    "revision": "e0e6a48831e0e3c07cc924b768a54d06",
+    "url": "/bakery/static/media/baker.e0e6a488.svg"
+  }
+]);
